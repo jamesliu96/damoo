cbdf22cad6a217e674ace0a4f08d94aee8d6755d
James Liu <jamesleu96@gmail.com>
2015-04-25 16:26:07 +0800

    v1.3.0: optimized code stucture for v2.0

--- damoo.js
+++ damoo.js
@@ -1,5 +1,5 @@
 /*!
- * Damoo - HTML5 Danmaku Engine v1.2.4
+ * Damoo - HTML5 Danmaku Engine v1.3.0
  * https://github.com/jamesliu96/Damoo
  *
  * Copyright (c) 2015 James Liu
@@ -7,41 +7,14 @@
  */
 ;(function(window) {
     var Damoo = function(m, n, r) {
-        if(!(this instanceof Damoo)) {
+        if (!(this instanceof Damoo)) {
             return new Damoo(m, n, r);
         }
-
-        this.parent = Damoo.dom.getElementById(m);
-        this.parent.style.position = "relative";
-
-        this.canvas = Damoo.dom.createElement('canvas');
-
-        this.canvas.id = n;
-
-        this.canvas.width = this.parent.offsetWidth;
-        this.canvas.height = this.parent.offsetHeight;
-
-        this.canvas.style.display = 'block';
-        this.canvas.style.backgroundColor = 'transparent';
-        this.canvas.style.position = 'absolute';
-        this.canvas.style.left = 0;
-        this.canvas.style.top = 0;
-        this.canvas.style.zIndex = 99999;
-
-        this.ctx = this.canvas.getContext('2d');
-
-        this.rows = r;
-
-        if (this.canvas.height / this.rows < 12) {
-            this.rows = this.canvas.height / 12;
-        }
-
-        this.font = new Font(this.canvas.height / this.rows, "Arial");
-
+        this.canvas = new Canvas(m, n, r);
         this.thread = new Thread();
     };
 
-    Damoo.version = "v1.2.4";
+    Damoo.version = "v1.3.0";
 
     Damoo.dom = window.document;
 
@@ -50,7 +23,7 @@
     Damoo.utils.preload = function(d, f) {
         var cvs = Damoo.dom.createElement('canvas'),
             ctx = cvs.getContext('2d');
-        cvs.width = f.size * d.text.length;
+        cvs.width = f.size * d.text.length * 1.2;
         cvs.height = f.size * 1.2;
         ctx.font = f.value;
         ctx.fillStyle = "#fff";
@@ -61,6 +34,18 @@
         return cvs;
     };
 
+    Damoo.utils.random = function(r) {
+        return Math.random() * r;
+    };
+
+    Damoo.utils.round = function(d) {
+        return d + 0.5 | 0;
+    };
+
+    Damoo.utils.floor = function(d) {
+        return d | 0;
+    };
+
     var RAF = window.requestAnimationFrame ||
         window.mozRequestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -69,21 +54,21 @@
         function(cb) { setTimeout(cb, 17); };
 
     Damoo.prototype.show = function() {
-        this.parent.appendChild(this.canvas);
+        this.canvas.parent.appendChild(this.canvas.layer);
     };
 
     Damoo.prototype.hide = function() {
-        this.parent.removeChild(this.canvas);
+        this.canvas.parent.removeChild(this.canvas.layer);
     };
 
     Damoo.prototype.emit = function(d) {
-        var cvs = Damoo.utils.preload(d, this.font);
+        var cvs = Damoo.utils.preload(d, this.canvas.font);
         this.thread.push({
             canvas: cvs,
             speed: Math.log(cvs.width),
             offset: {
                 x: this.canvas.width,
-                y: null
+                y: this.canvas.font.size * Damoo.utils.floor(Damoo.utils.random(this.canvas.rows))
             }
         });
     };
@@ -93,15 +78,11 @@
     };
 
     Damoo.prototype.start = function() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.canvas.clear();
         for (var i = 0; i < this.thread.length; i++) {
-            var x = (this.thread.get(i).offset.x + 0.5) | 0, y;
-            if (this.thread.get(i).offset.y == null) {
-                y = this.thread.get(i).offset.y = this.font.size * Math.floor(Math.random() * this.rows);
-            } else {
+            var x = this.thread.get(i).offset.x,
                 y = this.thread.get(i).offset.y;
-            }
-            this.ctx.drawImage(this.thread.get(i).canvas, x, y);
+            this.canvas.draw(this.thread.get(i), x, y);
             this.thread.get(i).offset.x -= this.thread.get(i).speed;
             if (x <= -this.thread.get(i).canvas.width) {
                 this.thread.remove(i);
@@ -112,6 +93,39 @@
                 self.start();
             };
         }(this));
+    };
+
+    var Canvas = function(m, n, r) {
+        this.parent = Damoo.dom.getElementById(m);
+        this.parent.style.position = "relative";
+        this.name = n;
+        this.rows = r;
+        if (this.height / this.rows < 12) {
+            this.rows = this.height / 12;
+        }
+        this.width = this.parent.offsetWidth;
+        this.height = this.parent.offsetHeight;
+        this.font = new Font(this.height / this.rows, "sans-serif");
+        this.layer = Damoo.dom.createElement('canvas');
+        this.context = this.layer.getContext('2d');
+        this.layer.className = this.name;
+        this.layer.id = Math.random().toString(16).substr(2).substr(0, 6);
+        this.layer.width = this.width;
+        this.layer.height = this.height;
+        this.layer.style.display = 'block';
+        this.layer.style.backgroundColor = 'transparent';
+        this.layer.style.position = 'absolute';
+        this.layer.style.left = 0;
+        this.layer.style.top = 0;
+        this.layer.style.zIndex = 99999;
+    };
+
+    Canvas.prototype.clear = function() {
+        this.context.clearRect(0, 0, this.width, this.height);
+    };
+
+    Canvas.prototype.draw = function(t, x, y) {
+        this.context.drawImage(t.canvas, Damoo.utils.round(x), Damoo.utils.round(y));
     };
 
     var Font = function(s, f) {
@@ -126,28 +140,28 @@
     });
 
     var Thread = function() {
-        this.memory = [];
+        this.pool = [];
     };
 
-    Thread.prototype.push = function(dt) {
-        this.memory.push(dt);
+    Thread.prototype.push = function(d) {
+        this.pool.push(d);
     };
 
-    Thread.prototype.get = function(idx) {
-        return this.memory[idx];
+    Thread.prototype.get = function(d) {
+        return this.pool[d];
     };
 
-    Thread.prototype.remove = function(idx) {
-        this.memory.splice(idx, 1);
+    Thread.prototype.remove = function(d) {
+        this.pool.splice(d, 1);
     };
 
     Thread.prototype.empty = function() {
-        this.memory = [];
+        this.pool = [];
     };
 
     Object.defineProperty(Thread.prototype, 'length', {
         get: function() {
-            return this.memory.length;
+            return this.pool.length;
         }
     });
 
